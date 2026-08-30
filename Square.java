a074fe685b839c21292627103c4a34113b4e7c99
hariharan250120-prog <hariharan.250120@cse.ritchennai.edu.in>
2026-08-30 22:46:23 +0530

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,9 @@
+public class SquareNumber {
+    public static void main(String[] args) {
+        int number = 5;
+        int square = number * number;
+
+        System.out.println("Number = " + number);
+        System.out.println("Square = " + square);
+    }
+}
