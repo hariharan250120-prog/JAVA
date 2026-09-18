ce6043ca9acaab059afc2fdfc1f8dc107f7e77b3
hariharan250120-prog <hariharan.250120@cse.ritchennai.edu.in>
2026-09-18 21:19:23 +0530

    Create CountEvenOrOdd.java

---
+++ CountEvenOrOdd.java
@@ -0,0 +1,17 @@
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int even = 0;
+        int odd = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even numbers = " + even);
+        System.out.println("Odd numbers = " + odd);
+    }
+}
