08f56566853a226eb362a2003fda7891776e4ca9
hariharan250120-prog <hariharan.250120@cse.ritchennai.edu.in>
2026-09-20 22:07:54 +0530

    Create SmallestOf2Numbers.java

---
+++ SmallestOf2Numbers.java
@@ -0,0 +1,12 @@
+public class SmallestOfTwo {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 8;
+
+        if (a < b) {
+            System.out.println("Smallest = " + a);
+        } else {
+            System.out.println("Smallest = " + b);
+        }
+    }
+}
